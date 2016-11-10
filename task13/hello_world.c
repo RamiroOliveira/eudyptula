bbc47be3b3ac3963db77ab90fc6fad39e58a1f95
Ramiro Oliveira <Ramiro.Oliveira@synopsys.com>
2016-11-10 11:59:38 +0000

    Add task 13
    Signed-off-by: Ramiro Oliveira <Ramiro.Oliveira@synopsys.com>

---
+++ task13/hello_world.c
@@ -0,0 +1,94 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+struct identity {
+	char name[20];
+	int id;
+	bool busy;
+	struct list_head list;
+};
+
+static LIST_HEAD(identity_list);
+
+int identity_create(char *name, int id)
+{
+	struct identity *temp;
+
+	temp = kmalloc(sizeof(*temp), GFP_KERNEL);
+	if (!temp)
+		return -ENOMEM;
+
+	strncpy(temp->name, name, 20);
+	temp->id = id;
+	temp->busy = false;
+
+	INIT_LIST_HEAD(&temp->list);
+
+	list_add_tail(&temp->list, &identity_list);
+	return 0;
+}
+
+struct identity *identity_find(int id)
+{
+	struct identity *temp;
+
+	list_for_each_entry(temp, &identity_list, list) {
+		if (temp->id == id)
+			return temp;
+	}
+	return NULL;
+}
+
+void identity_destroy(int id)
+{
+	struct identity *temp, *next;
+
+	list_for_each_entry_safe(temp, next, &identity_list, list) {
+		if (temp->id == id) {
+			list_del(&temp->list);
+			kfree(temp);
+		}
+	}
+}
+
+static int __init hello_init(void)
+{
+	struct identity *temp;
+
+	identity_create("Alice", 1);
+	identity_create("Bob", 2);
+	identity_create("Dave", 3);
+	identity_create("Gena", 10);
+
+	temp = identity_find(3);
+	if (temp == NULL)
+		pr_debug("id 3 not found\n");
+	else
+		pr_debug("id 3 = %s\n", temp->name);
+
+	temp = identity_find(42);
+	if (temp == NULL)
+		pr_debug("id 42 not found\n");
+	else
+		pr_debug("id 42 = %s\n", temp->name);
+
+	identity_destroy(2);
+	identity_destroy(1);
+	identity_destroy(10);
+	identity_destroy(42);
+	identity_destroy(3);
+
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	return;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
